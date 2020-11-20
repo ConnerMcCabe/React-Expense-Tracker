ea91b391cd5002136b67e7c363e29088f7665ce0
Conner McCabe <conner.p.mccabe@gmail.com>
2020-11-19 18:27:19 -0600

    making some styling changes

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
   Keep track of income and expenses. Add and remove items and save to local storage
   
   ICEBOX: 
-  - adding a calendar component and a date aspect to the GlobalState
+  - adding a calendar component and a date aspect to the GlobalState {https://www.npmjs.com/package/react-calendar}
   - adding the ability to see trends overtime and tuck away transaction history beyond recent
+  
 ## Usage
 
 -`npm install`
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -6,9 +6,10 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li>
-      {transaction.text}<span>{sign}{Math.abs(transaction.amount)}$</span>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text}
       <button className="delete-btn">x</button>
+      <span>{sign}{Math.abs(transaction.amount)}$</span>
     </li>
   )
 };
