13a7accd9ade258570f5b693029a7c6e949b68aa
Conner McCabe <conner.p.mccabe@gmail.com>
2020-11-20 15:08:50 -0600

    adding transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
-  const [amount, setAmount] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: generateKey,
+      text,
+      amount: +amount
+    }
+    addTransaction(newTransaction);
+  }
+
+  const generateKey = () => {
+    return `$i_${new Date().getTime()}`;
+  }
+
   return (
     <>
       <h3>Add New transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label>Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -25,11 +25,19 @@ export const GlobalProvider = ({ children }) => {
       payload: id
     })
   }
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return(
     <GlobalContext.Provider 
       value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
       }}
     >
       {children}
