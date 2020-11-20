943427b4271768d73e8962bf59b78383c2108a9c
Conner McCabe <conner.p.mccabe@gmail.com>
2020-11-19 18:43:26 -0600

    dynamically displaying dummy expense and income

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount)
+
+  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+    
+    
   return (
     <div className="containerIncExp">
       <div>
         <h4>Income</h4>
-        <p id="plus">+$0.00</p>
+        <p id="plus">+${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p id="minus">-$0.00</p>
+        <p id="minus">-${expense}</p>
       </div>
     </div>
   )
